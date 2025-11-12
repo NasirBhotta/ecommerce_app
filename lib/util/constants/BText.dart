38a62982c9da0ec6ed463da59604394e8908440e
Nasir Bhutta <me.bhotta@gmail.com>
2025-11-12 17:34:35 +0500

    text constants created

---
+++ lib/util/constants/BText.dart
@@ -0,0 +1,17 @@
+/// Contains all app text in string format.
+class BTexts {
+  // GLOBAL TEXTS
+
+  // Onboarding Titles
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  // Onboarding Subtitles
+  static const String onBoardingSubTitle1 =
+      "Welcome to a world of limitless choices - your perfect product awaits.";
+  static const String onBoardingSubTitle2 =
+      "For seamless transactions, choose your payment path - your convenience, our priority.";
+  static const String onBoardingSubTitle3 =
+      "From our doorstep to yours - swift, secure, and contactless delivery.";
+}
